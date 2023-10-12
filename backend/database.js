f83f5cb6ffba6ee1b355d21dd08d946ece3da6d0
xDev2022-power <22comzace@gmail.com>
2023-10-12 13:23:48 +0800

    1012 123pm

---
+++ backend/database.js
@@ -0,0 +1,33 @@
+require('dotenv').config()
+var mysql = require("mysql2")
+
+
+
+function Comments_Nole () {
+
+    // Date : 10/12/23 
+    // Author : Nole
+    // Activities 
+    // Purpose : Create new database.js class
+    //           Initiate the DB Connection
+    //           var connection = mysql.createConnection
+    //
+    
+    //
+}
+
+
+
+
+
+var connection = mysql.createConnection(
+    {
+
+        host: process.env.HOST,
+        database: process.env.DATABASE,
+        user: process.env.MYDB_USERNAME,
+        password: process.env.PASSWORDASSET_MYSQL
+
+    });
+
+    module.exports = connection;
---
+++ backend/server.js
@@ -0,0 +1,52 @@
+require('dotenv').config()
+var express = require("express");
+
+var cors = require('cors');
+var mysql = require("mysql2");
+var connection = require('./database');
+const {randomUUID } = require('crypto')
+
+var app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/",(req,res) => 
+{
+    res.json("Hello Test")
+}
+)
+
+function Comments_Nole () {
+
+    // Date : 10/12/23 
+    // Author : Nole
+    // Activities 
+    // Purpose : Create new server.js class
+    //           Initiate the DB Connection
+    //           app.listen(process.env.PORT,() => {..}
+    //
+    
+    // Date : 
+    // Author : 
+    // Activities
+    // Purpose : 
+}
+
+app.listen(process.env.PORT,() => {
+    connection.connect(function(err)
+    {
+        if(err)
+        {
+            
+            console.log("No Database Present");
+        } else
+        {
+            console.log("Database Connected");
+            
+            console.log(`Server running at http://${process.env.HOSTNAME}:${process.env.PORT}/`)
+        }
+    }
+    )
+}
+);
