68274f8bda3316ee07421855db44b66dd7896505
unknown <117880307+jinshin19@users.noreply.github.com>
2023-10-12 16:20:44 +0800

    Jinshin: Made some changes and configuration

--- backend/server.js
+++ backend/server.js
@@ -1,38 +1,43 @@
+// Packages
 require('dotenv').config()
 var express = require("express");
-
 var cors = require('cors');
-var mysql = require("mysql2");
-var connection = require('./database');
 
+// Routes
+const usersRoutes = require('./routes/users_routes')
 
+// Server Initialization
 var app = express();
 
+// Middlewares
 app.use(cors());
 app.use(express.json());
 
 app.get("/",(req,res) => 
 {
     res.json("Hello Test")
-    
 }
 )
 
-function Comments () {
 
-    // Date : 10/12/23 
-    // Author : Nole
-    // Activities 
-    // Purpose : Create new server.js class
-    //           Initiate the DB Connection
-    //           app.listen(process.env.PORT,() => {..}
-    //
-    
-    // Date : 
-    // Author : 
-    // Activities
-    // Purpose : 
-}
+/** Comments
+ *  Date : 10/12/23 
+    Author : Nole
+    Activities 
+    Purpose : Create new server.js class
+            Initiate the DB Connection
+            app.listen(process.env.PORT,() => {..}
+
+
+    Date : 10 / 12 / 23
+    Author : jinshin
+    Activities
+    Purpose : 
+            - remove mysql package ( e.g: const mysql = require('mysql2') )
+            - imported userRoutes = const usersRoutes = require('./routes/users_routes')
+            - to use the userRoutes = app.use('/api', usersRoutes)
+
+ */
 
 app.listen(process.env.PORT,() => {
     connection.connect(function(err)
@@ -52,3 +57,6 @@ app.listen(process.env.PORT,() => {
     )
 }
 );
+
+// Api's Routes
+app.use('/api', usersRoutes)
