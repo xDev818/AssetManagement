664f0863e82f2c967d9f4c09a82dccd99dfd61c5
unknown <117880307+jinshin19@users.noreply.github.com>
2023-10-17 15:40:19 +0800

    Jinshin: Added Ontype helper and set to inputs

--- frontend/src/components/Utils/ontype_validate.js
+++ frontend/src/components/Utils/ontype_validate.js
@@ -6,6 +6,7 @@ class OnType_Validate {
 
    static network = "ERR_NETWORK"
    static bad_request = "ERR_BAD_REQUEST"
+   static emails = ["@gmail.com", "@yahoo.com"]
 
    static async username ( input, values ) {
 
@@ -78,12 +79,137 @@ class OnType_Validate {
 
    }
 
-   static password ( input, values ) {
+   static async email ( input, values ) {
 
+      try {
+
+         if ( values.length === 0 ) {
+
+            console.log("Email is required")
+            input.style.border = "1px solid red"
    
+         } else if ( values.length < 11 ) {
+
+            console.log("Email is invalid")
+            input.style.border = "1px solid red"
+
+         } else {
+
+            const request = await axios.post("/users/email/verify", { values })
+   
+            const response = await request.data
+   
+            if ( response.message.includes("Email is available") ) {
+
+               console.log(response.message)
+               input.style.border = "1px solid green"
+
+            }
+
+         }
+
+      } catch ( err ) {
+
+         const errorStatus = err.code
+
+         if ( errorStatus.includes(this.network) ) {
+
+            const verifyUserLogs = new Logs(
+               "DB",
+               "Signin",
+               "onChange, Ontype_Valid, /users/email/verify",
+               err,
+               ""
+            )
+            alert( verifyUserLogs.getMessage() )
+
+         }
+
+         if ( errorStatus.includes( this.bad_request ) ) {
+            
+            if ( err.response.data.message.includes("Email is already taken") ) {
+
+               console.log(err.response.data.message)
+               input.style.border = "1px solid orange"
+
+               const verifyUserLogs = new Logs(
+                  "Error",
+                  "Signin",
+                  "onChange, Ontype_Valid, /users/email/verify",
+                  err.response.data.message,
+                  ""
+               )
+
+               alert( verifyUserLogs.getMessage() )
+
+            }
+
+         }
+
+      }
 
    }
 
+   static password ( input, module, values ) {
+
+      if ( module.includes("signup") ) {
+
+         if( !values.length ) {
+
+            console.log("Password is required")
+            input.style.border = "1px solid red"
+
+         } else if ( values.length < 5 ) {
+
+            console.log("Password must be atleast 8 characters length")
+            input.style.border = "1px solid red"
+
+         } else {
+
+            input.style.border = "1px solid green"
+
+         }
+
+      } else {
+
+         if ( !values.length ) {
+
+            console.log("Password is required")
+            input.style.border = "1px solid red"
+
+         } else {
+
+            input.style.border = "1px solid transparent"
+
+         }
+
+      }
+
+   }
+
+   static confirm_password ( other_password_input, input, module, values ) {
+
+      if ( module.includes("signup") ) {
+
+         if ( !values.length ) {
+
+            console.log("Confirm password is required")
+            input.style.border = "1px solid red"
+
+         } else if ( values !== other_password_input.target.value ) {
+
+            console.log("Password is not match ")
+            input.style.border = "1px solid red"
+
+         } else {
+
+            input.style.border = "1px solid green"
+
+         }
+
+      }
+
+   }
 
 }
 
