e057991f5ae21cbbc51bac8771621ead7e33c58f
John Ogama <jogama696@gmail.com>
2023-10-17 10:30:38 +0800

    feat: output user profile info

--- frontend/src/components2/Profile/UpdateProfile.js
+++ frontend/src/components2/Profile/UpdateProfile.js
@@ -13,9 +13,25 @@ import {
   GridItem,
 } from "@chakra-ui/react";
 import Card from "components/Card/Card";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import decoder from "jwt-decode";
 
 export default function UpdateProfile() {
+  const [data, setData] = useState();
+  const token = window.localStorage.getItem("token");
+  useEffect(() => {
+    setData(decoder(token));
+  }, []);
+
+  // Destructuring personal info
+  const displayName = data.result[0].displayName;
+  const department = data.result[0].departmentName;
+  const email = data.result[0].email;
+  const userRole = data.result[0].userRole;
+  const profileImage = data.result[0].imgFilename;
+
+  console.log(profileImage);
+
   return (
     <Card
       w={{ base: "auto", md: "auto", lg: "auto" }}
@@ -30,27 +46,50 @@ export default function UpdateProfile() {
           flexDirection="column"
           alignItems="center"
         >
-          <Image
-            src="https://www.treasury.gov.ph/wp-content/uploads/2022/01/male-placeholder-image.jpeg"
+          <Box
             borderRadius="100%"
-            w={{ base: 100 }}
-          />
-          <Input type="file" mt={4} />
+            bg="blackAlpha.100"
+            w={150}
+            h={150}
+            overflow="hidden"
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+          >
+            {profileImage ? (
+              <Image
+                src={`http://localhost:5001/${profileImage}`}
+                w={{ base: 100 }}
+              />
+            ) : (
+              <Image src="https://www.treasury.gov.ph/wp-content/uploads/2022/01/male-placeholder-image.jpeg" />
+            )}
+          </Box>
+          <input type="file" mt={4} />
         </Flex>
         <Stack gap={2} mt={10}>
+          <FormLabel>Position: {userRole}</FormLabel>
+          <Box>
+            <FormLabel fontSize={{ base: "sm" }}>Department</FormLabel>
+            <Select placeholder={department}>
+              <option value="option1">IT</option>
+              <option value="option2">Admin</option>
+              <option value="option3">Support Desk</option>
+            </Select>
+          </Box>
           <Grid templateColumns="repeat(2, 1fr)" gap={5}>
             <GridItem>
               <FormLabel fontSize={{ base: "sm" }}>First Name</FormLabel>
-              <Input value="John" />
+              <Input value={displayName} />
             </GridItem>
             <GridItem>
               <FormLabel fontSize={{ base: "sm" }}>Last Name</FormLabel>
-              <Input value="Cena" />
+              <Input value={displayName} />
             </GridItem>
           </Grid>
           <Box>
             <FormLabel fontSize={{ base: "sm" }}>Email Address</FormLabel>
-            <Input />
+            <Input value={email} />
           </Box>
           <Box>
             <FormLabel fontSize={{ base: "sm" }}>Password</FormLabel>
@@ -60,14 +99,7 @@ export default function UpdateProfile() {
             <FormLabel fontSize={{ base: "sm" }}>Confirm Password</FormLabel>
             <Input type="password" />
           </Box>
-          <Box>
-            <FormLabel fontSize={{ base: "sm" }}>Department</FormLabel>
-            <Select>
-              <option value="option1">IT</option>
-              <option value="option2">Admin</option>
-              <option value="option3">Support Desk</option>
-            </Select>
-          </Box>
+
           <Box>
             <Button colorScheme="green">Update Profile</Button>
           </Box>
