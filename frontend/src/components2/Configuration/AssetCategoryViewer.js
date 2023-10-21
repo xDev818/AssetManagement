8cf8189faaa370d3182b3b4d1812ff2c47e203f6
xDev2022-power <22comzace@gmail.com>
2023-10-22 04:14:07 +0800

    1022 4414am

--- frontend/src/components2/Configuration/AssetCategoryViewer.js
+++ frontend/src/components2/Configuration/AssetCategoryViewer.js
@@ -74,7 +74,7 @@ export default function AssetCategoryViewer() {
   const currentNumber = (number) => {
     setCurrentPage(number);
   };
-
+ 
   useEffect(() => {
     LoadAllCategories();
   }, []);
